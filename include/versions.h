8656cfa00ce0013916e2c1f2960378264405da50
Marco Scholl <develop@marco-scholl.de>
2023-08-06 04:29:52 +0200

    wip

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#define MAIN_Version "89932f2"
-#define KNX_Version "2934782"
-#define MODULE_Common_Version "1.0.0+8ee24df"
+#define MAIN_Version "a82e940"
+#define KNX_Version "f7ac39c"
+#define MODULE_Common_Version "1.0.0+c7ccbb7"
 #define MODULE_LogicModule_Version "4.0.0+73cac2f"
+#define MODULE_Updater_Version "0.0.0+0fa5d39"
--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,13 @@ void setup()
     openknx.addModule(9, new UpdaterModule());
 #endif
     openknx.setup();
+    openknx.progLed.blinking();
+#ifdef ARDUINO_ARCH_ESP32
+    logInfo("Flash", "Size %i", spi_flash_get_chip_size());
+    test = new OpenKNX::Flash::Driver(0x2b0000, 0x2000, "Test");
+    test->eraseSector(0);
+#endif
+}
 
     // openknx.progLed.off();
     // openknx.progLed.on();
@@ -31,25 +38,16 @@ uint8_t counterTable[] = {255, 254, 252, 248, 240, 224, 192, 128, 0};
 uint8_t counter = 0;
 void loop()
 {
-
-    if (delayCheck(x1, 1000))
-    {
-
-        logInfo("Counter", "%i %i", counter, counterTable[counter]);
-
-        uint8_t reCounter = counterTable[counter];
-        uint8_t index = std::distance(std::begin(counterTable), std::find(std::begin(counterTable), std::end(counterTable), reCounter));
-        logInfo("ReCounter", "%i %i", reCounter, index);
-
-        counter++;
-        if (counter > 8) counter = 0;
-        x1 = millis();
-        // openknx.flashOpenKNX.write(0, 0x10, 100);
-        // openknx.flashOpenKNX.write(10, 0x20, 80);
-        // openknx.flashOpenKNX.commit();
-        // logHexInfo("FlashContent", openknx.flashOpenKNX.flashAddress(), 256);
-    }
-
+#ifdef ARDUINO_ARCH_ESP32
+    // if (delayCheck(x1, 5000))
+    // {
+    //     x1 = millis();
+    //     test->write(x2 , (uint8_t)x1, 1);
+    //     test->commit();
+    //     logHexInfo("FlashContent", test->flashAddress(), 256);
+    //     x2 += 1;
+    // }
+#endif
     openknx.loop();
 }
 
