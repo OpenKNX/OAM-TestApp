4b263fe85cbc67c3e0ed138a48b7c77bb3781ae2
Marco Scholl <develop@marco-scholl.de>
2023-08-08 01:25:04 +0200

    wip

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "7bb22ee"
+#define MAIN_Version "f5a9dcd"
 #define KNX_Version "2934782"
-#define MODULE_Common_Version "1.0.0+e57c6d2"
+#define MODULE_Common_Version "1.0.0+43c3dc8"
 #define MODULE_LogicModule_Version "4.0.0+73cac2f"
