6455cfd88497c57e363ec33feaec09f1ef193937
Marco Scholl <develop@marco-scholl.de>
2023-08-11 08:20:57 +0200

    wip

--- include/versions.h
+++ include/versions.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#define MAIN_Version "a82e940"
-#define KNX_Version "f7ac39c"
-#define MODULE_Common_Version "1.0.0+c7ccbb7"
+#define MAIN_Version "8656cfa"
+#define KNX_Version "2934782"
+#define MODULE_Common_Version "1.0.0+8ee24df"
 #define MODULE_LogicModule_Version "4.0.0+73cac2f"
-#define MODULE_Updater_Version "0.0.0+0fa5d39"
--- src/main.cpp
+++ src/main.cpp
@@ -20,31 +20,22 @@ void setup()
     openknx.addModule(9, new UpdaterModule());
 #endif
     openknx.setup();
-    openknx.progLed.blinking();
-#ifdef ARDUINO_ARCH_ESP32
-    logInfo("Flash", "Size %i", spi_flash_get_chip_size());
-    test = new OpenKNX::Flash::Driver(0x2b0000, 0x2000, "Test");
-    test->eraseSector(0);
-#endif
-}
 
     // openknx.progLed.off();
     // openknx.progLed.on();
     // openknx.progLed.blinking();
     openknx.progLed.pulsing();
 }
-// bool d = false;
-uint8_t counterTable[] = {255, 254, 252, 248, 240, 224, 192, 128, 0};
-uint8_t counter = 0;
+
 void loop()
 {
 #ifdef ARDUINO_ARCH_ESP32
     // if (delayCheck(x1, 5000))
     // {
     //     x1 = millis();
-    //     test->write(x2 , (uint8_t)x1, 1);
-    //     test->commit();
-    //     logHexInfo("FlashContent", test->flashAddress(), 256);
+    //     other->write(x2 , (uint8_t)x1, 1);
+    //     other->commit();
+    //     logHexInfo("FlashContent", other->flashAddress(), 256);
     //     x2 += 1;
     // }
 #endif
