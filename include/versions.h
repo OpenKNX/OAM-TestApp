12b44d4d6bd7c0fe2a94bd431de3ea8c10153f2e
Marco Scholl <develop@marco-scholl.de>
2023-08-12 23:56:49 +0200

    add versions.h to git ignore

--- include/versions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define MAIN_Version "6455cfd"
-#define KNX_Version "6322825"
-#define MODULE_Common_Version "1.0.0+3b112a0"
-#define MODULE_LogicModule_Version "4.0.0+73cac2f"
-#define MODULE_Updater_Version "0.0.0+0fa5d39"
