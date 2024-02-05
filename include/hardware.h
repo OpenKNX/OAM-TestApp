ea4330dac52bf80b7e7e02c54a69efa74c340d3f
Ing-Dom <dom@ing-dom.de>
2024-02-05 16:02:44 +0100

    added WT32

--- include/hardware.h
+++ include/hardware.h
@@ -48,4 +48,19 @@
         #define PROG_BUTTON_PIN_INTERRUPT_ON FALLING
         #define HARDWARE_NAME "ESP32_DEV"
     #endif
+    #ifdef BOARD_ESP32_WT32_ETH01
+        #define PROG_LED_PIN 2
+        #define PROG_LED_PIN_ACTIVE_ON HIGH
+        #define PROG_BUTTON_PIN 4
+        #define PROG_BUTTON_PIN_INTERRUPT_ON FALLING
+
+        #define ETH_PHY_TYPE        ETH_PHY_LAN8720
+        #define ETH_PHY_ADDR        1
+        #define ETH_PHY_MDC         23
+        #define ETH_PHY_MDIO        18
+        #define ETH_PHY_POWER       16
+        #define ETH_CLK_MODE        ETH_CLOCK_GPIO0_IN
+
+        #define HARDWARE_NAME "ESP32_WT32_ETH01"
+    #endif
 #endif
